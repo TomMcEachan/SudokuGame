7c50d0782d65400c66611fdbc534788dce9e2f48
Tom McEachan <tom.a.mceachan@outlook.com>
2022-02-17 21:13:27 +0000

    Initial configuration

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace SudokuGame
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
