d514b5f7f916ca6343409690a8aab512f73c886f
Tom McEachan <tom.a.mceachan@outlook.com>
2022-02-21 20:43:13 +0000

    Added logic to solve a sudoku puzzle

--- Solve.cs
+++ Solve.cs
@@ -9,17 +9,74 @@ namespace SudokuGame
     public class Solve
     {
 
-       public bool SolveBoard(int row, int column, int[,] grid)
-        {
 
-        } 
-
-        public bool findEmpty (int row, int column, int num)
+        //This method checks if the grid is empty. If it is not empty it solves the grid.
+        public void CheckSolve(int[,] grid)
         {
-            for (row=0; row < Sudoku.easy; row++)
+            if (grid.Length == 0)
             {
-               for (column=0; row < )
+                return;
             }
+            SolveGrid(grid);
         }
+
+
+        //This method solves the grid
+        public static bool SolveGrid(int[,] grid)
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (grid[x, y] == 0)
+                    {
+                        for (int z = 1; z <= 9; z++)
+                        {
+                            if (isValid(grid, x, y, z))
+                            {
+                                grid[x, y] = z;
+
+                                if (SolveGrid(grid))
+                                    return true;
+                                else
+                                    grid[x, y] = 0;
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+        //This method checks if a row or column is valid
+        private static bool isValid(int[,] grid, int row, int column, int size)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                //check row  
+                if (grid[x, column] != 0 && grid[x, column] == size)
+                    return false;
+                //check column  
+                if (grid[row, x] != 0 && grid[row, x] == size)
+                    return false;
+                //check 3*3 block  
+                if (grid[3 * (row / 3) + x / 3, 3 * (column / 3) + x % 3] != 0 && grid[3 * (row / 3) + x / 3, 3 * (column / 3) + x % 3] == size)
+                    return false;
+            }
+            return true;
+        }
+
+
+
+
+
+
+
+
     }
+
 }
+
+
--- Sudoku.cs
+++ Sudoku.cs
@@ -14,7 +14,8 @@ namespace SudokuGame
         public static int medium = 12;
         public static int hard = 14;
         public static int extreme = 18;
-
+        public static int selectedModeValue;
+        public static int selectedModeInt;
 
         public static int [,] emptyGrid =
             {
@@ -29,14 +30,28 @@ namespace SudokuGame
             {0, 0, 0, 1, 0, 0, 2, 0, 4}
             };
 
-        static void printBoard()
+        public static int [,] testGrid =
+            {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 6, 0, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+            };
+
+        public static int[,] generatedGrid;
+        static void printBoard(int[,] grid)
         {
             Console.WriteLine("+-----+-----+-----+");
 
             for (int x = 1; x < 10; x++)
             {
                 for (int y = 1; y < 10; y++)
-                    Console.Write("|{0}", emptyGrid[x - 1, y - 1]);
+                    Console.Write("|{0}", grid[x - 1, y - 1]);
 
                 Console.WriteLine("|");
                 if (x % 3 == 0) Console.WriteLine("+-----+-----+-----+");
@@ -48,26 +63,62 @@ namespace SudokuGame
             Console.WriteLine(FiggleFonts.Doom.Render("Sudoku Generator!"));
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Welcome! This program generates unique, solveable Sudoku puzzles.");
-            Console.WriteLine("To generate a puzzle, please select a difficulty level from the following options (please select 1, 2, 3, or 4) \n\n" +
+            Console.WriteLine("To generate a puzzle, please select a difficulty level from the following options by typing 1, 2, 3, or 4.\n\n" +
                                 " 1 - Easy (Produces a simple 9x9 Sudoku puzzle to solve)\n" +
                                 " 2 - Medium (Produces a 12x12 Sudoku puzzle to solve)\n" +
                                 " 3 - Hard (Produces a 14x14 Sudoku puzzle to solve)\n" +
-                                " 4 - Extreme (Produces a 18x18 Sudoku puzzle to solve");
-            Console.WriteLine();
+                                " 4 - Extreme (Produces a 18x18 Sudoku puzzle to solve\n\n");
+            
             String selectedModeString = Console.ReadLine();
-            int selectedModeInt = Convert.ToInt32(selectedModeString);
-            return selectedModeInt;
+            int selectedModeNumber = Convert.ToInt32(selectedModeString);
+            return selectedModeNumber;
+        }
+
+
+        static void setSelectedOption(int selectedMode)
+        {       
+            if (selectedMode == 1)
+            {
+                selectedModeValue = easy;
+                Console.WriteLine("Generating an easy Sudoku puzzle....");
+            }
+            else if (selectedMode == 2)
+            {
+                selectedModeValue = medium;
+                Console.WriteLine("Generating a medium Sudoku puzzle....");
+            }
+            else if (selectedMode == 3)
+            {
+                selectedModeValue = hard;
+                Console.WriteLine("Generating a hard Sudoku puzzle....");
+            }
+            else if (selectedMode == 4)
+            {
+                selectedModeValue = extreme;
+                Console.WriteLine("Generating an extreme Sudoku puzzle....");
+            } 
+            else
+            {
+                selectedModeValue = easy;
+                Console.WriteLine("Generating a standard Sudoku puzzle....");
+            }
+
         }
         static void Main(string[] args)
         {
 
             Generate generate = new Generate();
-            Solve solve = new Solve();
+            
             int selected = printIntro();
+            setSelectedOption(selected);
+            printBoard(testGrid);
 
-            solve.SolveBoard(0, 0, emptyGrid);
-            generate.GenerateGrid(selected);
-            printBoard();
+            Console.WriteLine("Do you want to solve this board?");
+            Console.ReadLine();
+
+            Solve.SolveGrid(testGrid);
+            //generate.GenerateGrid(selected);
+            printBoard(testGrid);
             Console.ReadLine();
 
         }
